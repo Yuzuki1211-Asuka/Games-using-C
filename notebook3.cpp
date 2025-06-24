c78a4ee44fd40136348ac1f97088a4dfee77c642
Yuzuki1211-Asuka <1981954338@qq.com>
2025-06-24 09:04:39 +0800

    Add files via upload

---
+++ notebook3.cpp
@@ -0,0 +1,147 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<windows.h>
+int h,w;
+int bx,by;
+int vx,vy;
+int px,py;//center of plank
+int r;//ridus of plank
+int pl,pr;//left and right side of plank
+int num;//times of bouncing balls
+int b_x,b_y;//position of blocks
+int score;
+void HideCursor()//hiding cursor
+{
+	CONSOLE_CURSOR_INFO cursor_info={1,0};
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&cursor_info);
+}
+void gotoxy(int x,int y)//controlling cursor
+{
+	HANDLE handle=GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X=x;
+	pos.Y=y;
+	SetConsoleCursorPosition(handle,pos);
+}
+void startup()//initializing data
+{
+	h=15;
+	w=20;
+	bx=0;
+	by=w/2;
+	vx=1;
+	vy=1;
+	r=6;
+	px=h,py=w/2;
+	pl=py-r;
+	pr=py+r;
+	num=0;
+	b_x=0;
+	b_y=w/2+1;
+	score=0;
+}
+void show()//initializing startup surface
+{
+	gotoxy(0,0);
+	for(int i=0;i<=h+1;i++)
+	{
+		for(int j=0;j<=w;j++)
+		{
+			if((i==bx)&&(j==by))
+			{
+				printf("o");
+			}
+			else if(j==w)
+			{
+				printf("|");
+			}
+			else if(i==h+1)
+			{
+				printf("-");
+			}
+			else if((i==h)&&(j>=pl)&&(j<=pr))
+			{
+				printf("*");
+			}
+			else if((i==b_x)&&(j==b_y))
+			{
+				printf("B");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	printf("The number of bouncing balls: %d\n",num);
+	printf("The number of disappearing blocks: %d\n",score);
+}
+void updatenoinput()
+{
+	if(bx==h-1)
+	{
+		if((by>=pl)&&(by<pr))
+		{
+			num++;
+			//printf("\a");
+			b_y=b_y+rand()%4-2;
+		}
+		else
+		{
+			printf("Game over.\n");
+			system("pause");
+			exit(0);
+		}
+	}
+	if((bx==b_x)&&(by==b_y))
+	{
+		score++;
+		b_y=rand()%w;//generating new blocks 
+	}
+	bx+=vx;
+	by+=vy;
+	if((bx==0)||(bx==h-1))
+	{
+		vx=-vx;
+	}
+	if((by==0)||(by==w-1))
+	{
+		vy=-vy;
+	}
+	Sleep(50);
+}
+void updateinput()
+{
+	char input;
+	if(kbhit())
+	{
+		input=getch();
+		if((input=='a')||(input==75))//adding a new controling method
+		{
+			py--;
+			pl=py-r;
+			pr=py+r;
+		}
+		if((input=='d')||(input==77))
+		{
+			py++;
+			pl=py-r;
+			pr=py+r;
+		}
+	}
+}
+int main()
+{
+	startup();
+	system("cls");
+	HideCursor();
+	while(1)
+	{
+		show();
+		updatenoinput();
+		updateinput();
+	}
+	return 0;
+}
